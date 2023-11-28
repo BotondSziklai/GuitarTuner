e96d68ea0818d12e2d6795c151de7520943f2b85
BotondSziklai <sziklai709@gmail.com>
2023-11-28 22:49:50 +0100

    updated code

--- GT_Code/GT_Code.ino
+++ GT_Code/GT_Code.ino
@@ -44,7 +44,7 @@ void loop()
       lcd.setCursor(8,0);
       lcd.print("E -");
     }
-    else if(frek > 270 && frek < 322){
+    else if(frek > 269 && frek < 322){
       lcd.setCursor(8,0);
       lcd.print("E +");
     }
@@ -62,12 +62,83 @@ void loop()
       lcd.setCursor(8,0);
       lcd.print("B -");
     }
-    else if(frek > 215 && frek < 239){
+    else if(frek > 214 && frek < 239){
       lcd.setCursor(8,0);
       lcd.print("B +");
     }
   }
 
+//G string 
+  if(frek > 170 && frek < 215 ){
+    if(frek > 189 && frek < 201){
+      lcd.setCursor(8,0);
+      lcd.print("G-hur");
+      lcd.setCursor(3,1);
+      lcd.print("Behangolva");
+    }
+    else if(frek > 200 && frek < 215){
+      lcd.setCursor(8,0);
+      lcd.print("G -");
+    }
+    else if(frek > 169 && frek < 188){
+      lcd.setCursor(8,0);
+      lcd.print("G +");
+    }
+  }
+
+  //D string 
+  if(frek > 125 && frek < 170 ){
+    if(frek > 140 && frek < 152){
+      lcd.setCursor(8,0);
+      lcd.print("B-hur");
+      lcd.setCursor(3,1);
+      lcd.print("Behangolva");
+    }
+    else if(frek > 151 && frek < 170){
+      lcd.setCursor(8,0);
+      lcd.print("B -");
+    }
+    else if(frek > 124 && frek < 139){
+      lcd.setCursor(8,0);
+      lcd.print("B +");
+    }
+  }
+
+  //A string 
+  if(frek > 100 && frek < 125 ){
+    if(frek > 104 && frek < 116){
+      lcd.setCursor(8,0);
+      lcd.print("A-hur");
+      lcd.setCursor(3,1);
+      lcd.print("Behangolva");
+    }
+    else if(frek > 115 && frek < 125){
+      lcd.setCursor(8,0);
+      lcd.print("A -");
+    }
+    else if(frek > 100 && frek < 103){
+      lcd.setCursor(8,0);
+      lcd.print("A +");
+    }
+  }
+
+  //E* string 
+  if(frek > 70 && frek < 100 ){
+    if(frek > 76 && frek < 88){
+      lcd.setCursor(8,0);
+      lcd.print("E*-hur");
+      lcd.setCursor(3,1);
+      lcd.print("Behangolva");
+    }
+    else if(frek > 87 && frek < 100){
+      lcd.setCursor(8,0);
+      lcd.print("E* -");
+    }
+    else if(frek > 75 && frek < 70){
+      lcd.setCursor(8,0);
+      lcd.print("E* +");
+    }
+  }
    delay(200); //Delay
    lcd.clear(); //Clear display
 }
